7b4e8acd9974bebff9e4421c816f94fbdeb33d2f
Tulsi_Naik <tulsivnaik@gmail.com>
2025-07-03 22:15:38 +0530

    login 5

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import jwtDecode from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
+
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
