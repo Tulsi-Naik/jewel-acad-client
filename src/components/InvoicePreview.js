8096cc43ccda2a6a9e54369db57518233005e0cc
Tulsi_Naik <tulsivnaik@gmail.com>
2025-08-06 22:21:36 +0530

    bg image svg 7

--- src/components/InvoicePreview.js
+++ src/components/InvoicePreview.js
@@ -14,7 +14,7 @@ const InvoicePreview = forwardRef(({ customer = {}, saleItems = [], products = [
 
   const date = new Date();
   const currentDate = `${String(date.getDate()).padStart(2, '0')}/${String(date.getMonth() + 1).padStart(2, '0')}/${date.getFullYear()}`;
-
+//
   return (
     <div
       ref={ref}
